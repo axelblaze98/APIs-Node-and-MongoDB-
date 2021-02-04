5b5b1a5edbc52f64bde5f9ab17519d1bf7d30460
axelblaze98 <paarthbhandari98@gmail.com>
2021-02-05 01:47:47 +0530

    Chenges in process endpoint

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,11 +1,28 @@
 const routes=require('express').Router();
-const test = require('./test');
 
-routes.all('/', (req, res)=>{
+randomNumber=(max,min)=>{
+    return Math.floor(
+        Math.random()*(max-min+1)+min
+    );
+}
+getCurrentISTDate=()=>{
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    return (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+}
+routes.all('/process', (req, res)=>{
     if(req.method=='GET' || req.method=='POST' || req.method=='DELETE' || req.method=='PUT'){
-        res.status(400).send(test.test());
+        
+        var currentDate = getCurrentISTDate();
+        waitTime = randomNumber(15000,30000);
+        setTimeout(()=>{
+            res.status(200).send(currentDate+'+0000'+'  '+waitTime);
+        },waitTime);
     }
-    else res.status(200).send('access denied')
+    else res.status(400).send('access denied')
+})
+
+routes.get('/stats', (req, res)=>{
+    res.send('stats');
 })
 
 module.exports=routes;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const route = require('./Routes/routes');
 
-app.use('/api',route);
+app.use('/',route);
 
 app.listen(8000,()=>{
     console.log('Server listening on port 8000');
